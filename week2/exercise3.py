ed2893817fc266ec165a61467d0e95c098e8900b
Darrell <darrellcyl@hotmail.com>
2017-03-16 09:56:09 +1100

    booya

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -128,7 +128,15 @@ def loops_3():
     TIP: notice that this needs to to return strings of numbers,
          so call str(number) to cast.
     """
-    pass
+    number_square = []
+    for i in range(10):
+        number_row = []
+        for j in range(10):
+            number_row.append(i)
+        number_square.append(number_row)
+
+    print(number_square)
+    return number_square
 
 
 def loops_4():
