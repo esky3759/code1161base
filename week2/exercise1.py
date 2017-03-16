05ece12ca7f7a8679246cac277fe3f2b3b6e3752
Stephen <stephenkarahalias@gmail.com>
2017-03-16 16:07:36 +1100

    clown

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -1,4 +1,4 @@
-"""Tidy up this file.
+"""Tidyupthisfile.
 
 This file runs, but it's a mess!
 Go through it and change it until there are no more linter errors or warnings.
@@ -7,15 +7,16 @@ Make sure that your code still runs without any errors by pressing
 """
 from __future__ import division
 from __future__ import print_function
-print  ("hello! Let's get started")
-jobs=['get','this',
-'file','to','pass',
-  'the','linter']
-InOtherWords="make it show no linter errors"
+print ("hello! Let's get started")
+jobs = ['get', 'this', 'file', 'to', 'pass', 'the', 'linter']
+InOtherWords = "make it show no linter errors"
 import os
 print(jobs)
 print(InOtherWords)
-print(1+1,"is smaller than",7*0.5,"is",(1+1)<(7*0.5),", which is a relief!")
-def usefulFunction () :
+print(1+1, "is smaller than", 7*0.5, "is", (1+1) < (7*0.5), ", is a relief!")
+
+
+def usefulFunction():
+    """Print the current working directory."""
     print(os.getcwd())
-usefulFunction( )
+usefulFunction()
