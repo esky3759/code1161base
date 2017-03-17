ba8f49e93658780ae7eef2ceb37468cda7facbc0
Stephen <stephenkarahalias@gmail.com>
2017-03-17 11:26:37 +1100

    clown

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -39,7 +39,11 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    pass
+    star_list = []
+    for i in range(10):
+        star_list.append("*")
+
+        return star_list
 
 
 def star_map():
