26deb38c7f803def74a62489a371a7515ba76421
Stephen <stephenkarahalias@gmail.com>
2017-05-25 16:41:42 +1000

    clown

--- week3/exercise3.py
+++ week3/exercise3.py
@@ -2,8 +2,11 @@
 
 Steps on the way to making your own guessing game.
 """
+
 from __future__ import division
 from __future__ import print_function
+from exercise1 import not_number_rejector
+
 import random
 
 
@@ -19,53 +22,46 @@ def advancedGuessingGame():
     * chastise them if they pick a number outside the bounds.
     * see if you can find the other failure modes.
       There are three that I can think of. (They are tested for.)
-
     NOTE: whilst you CAN write this from scratch, and it'd be good for you to
     be able to eventually, it'd be better to take the code from exercise 2 and
     marge it with code from excercise 1.
     Remember to think modular. Try to keep your functions small and single
     purpose if you can!
     """
-    print("\nwelcome to the guessing game!")
-    print("A number between 0 and _ ?")
-    upperBound = raw_input("Enter an upper bound: ")
-    print("OK then, a number between 0 and {} ?".format(upperBound))
-    upperBound = int(upperBound)
-    actualNumber = random.randint(0, upperBound)
-
-    guessed = False
-
-    while not guessed:
-        guessedNumber = int(raw_input("guess a number: "))
-        print("you guessed {},".format(guessedNumber),)
-        if guessedNumber == actualNumber:
-            print("you got it!! It was {}".format(actualNumber))
-            guessed = True
-        elif guessedNumber < actualNumber:
-            print("too small, try again ")
-        else:
-            print("too big, try again   ")
-    return "You got it!"
-
-    lowerBound = raw_input("Enter an lower bound: ")
-    print("OK then, a number between 0 and {} ?".format(lowerBound))
+    print("\nWelcome to Guessing Game")
+    lowerBound = not_number_rejector("Enter a lower bound:")
+    print("Now enter the upper bound:")
+    upperBound = not_number_rejector("Enter an upper bound:")
+    while upperBound <= lowerBound:
+        print("re-enter upper bound")
+        upperBound = not_number_rejector("Enter an upper bound:")
+    pass
+    print("then enter a number between {} and {}?".format(lowerBound,
+                                                          upperBound))
     lowerBound = int(lowerBound)
-
-    actualNumber = random.randint(2, lowerBound)
-
+    upperBound = int(upperBound)
+    actualNo = random.randint(lowerBound, upperBound)
     guessed = False
 
     while not guessed:
-        guessedNumber = int(raw_input("guess a number: "))
-        print("you guessed {},".format(guessedNumber),)
-        if guessedNumber == actualNumber:
-            print("you are a genius! It was {}".format(actualNumber))
-            guessed = True
-        elif guessedNumber < actualNumber:
-            print("too small, try again ")
-        else:
-            print("too big, try again   ")
+        try:
+            guessedNo = int(raw_input("guess a number:"))
+            print("you guessed {},".format(guessedNo),)
+            if guessedNo == actualNo:
+                print("you got it! it was {}".format(actualNo))
+                guessed = True
+            elif guessedNo < lowerBound:
+                print("out of range")
+            elif guessedNo > upperBound:
+                print("out of range")
+            elif guessedNo < actualNo:
+                print("too small, try again...")
+            else:
+                print("too big, try again...")
+        except Exception as e:
+            print("good one...that is not a number...try again".format(e))
     return "You are a genius!"
+    pass
 
 
 if __name__ == "__main__":
